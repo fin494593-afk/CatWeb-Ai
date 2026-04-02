9d037c0a5dfa5d1734a9aa82927649a7fbef2247
fin494593-afk <fin494593@gmail.com>
2026-04-01 20:34:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 app = Flask(__name__)
 
 GEMINI_API_KEY = "AIzaSyD3FsgJlJ4JFlnWj1monbRWC5E6ALeyM_g"
-GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
+GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
 
 pending_prompt = None
 pending_response = None
